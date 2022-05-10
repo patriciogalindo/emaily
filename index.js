38185a4469ec93b6d6e85d9cbe532c6f755393b0
Patricio Galindo <patriciogalindo@gmail.com>
2022-05-10 18:44:32 -0400

    ab

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(cookieSession({
-    maxAge: 30*24*60*60*1000,
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
-)
-
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
